0e6fee78db77f1124e761141ceef89ec347af4a9
Alex <temper8@gmail.com>
2017-05-02 03:15:27 +0300

    добавил конструктор и переменные

--- linesplay.pas
+++ linesplay.pas
@@ -7,13 +7,32 @@ interface
 uses
   Classes, SysUtils;
 
+type
+
+TLinesPlay = class
+public
+  { public declarations }
+  x_size,y_size:integer;
+  sf : array[0..10,0..10] of integer;
+  board : array[0..10,0..10] of integer;
+  startSquare : TPoint;
+  endSquare : TPoint;
+  counter: integer;
+  path : array of TPoint;
+  clearBallsCount:integer;
+  clearBalls : array[0..100] of TPoint;
+
+  constructor Create();
+end;
+
 implementation
-  type
 
-  TGameBoard = class
-      public
-    { public declarations }
+constructor TLinesPlay.Create();
 
-  end;
+begin
+
+  x_size := 9;
+  y_size := 9;
+end;
 end.
 
